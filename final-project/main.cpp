1814b5f165b190c02c44f3c999eb1def32a97509
kboygit <heybisayangdako@gmail.com>
2017-06-06 00:03:50 -0700

    refactored

--- final-project/main.cpp
+++ final-project/main.cpp
@@ -19,11 +19,19 @@
  function N - Sort on last names.
  function C - Sort on city.
  function B - Sort on balance.
- function V - Print a customerÃs details in a pag`e format to the monitor.
+ function V - Print a customer√s details in a pag`e format to the monitor.
  function X - Exit the program.
 
- - NEED TO ADD A DETAILED DESCRIPTION -
-
+ Extra Credit:
+ 1) Add a search function that is not case sensitive and/or partial name search. (10 Pts.)
+ a. Make the search partial search, so if part of the name is given locate all names that contain the
+ sub string. (10 more points)
+ 2) Update and save the input file before stopping the program. Prompt user for a new file name. (10
+ Pts.)
+ 3) Develop a structure to represent a Bank (a sample form of the structure): (20 Pts.)
+ Name (Bank name)
+ List of accounts (Array of Customer type, assume size 50) Total Assets (Assets of the bank is total of all the deposits) + all the necessary utility functions (read, print, process,...)
+ 4) Add any features that may improve the program. (Email me first to make sure it is worth extra credit)
  ************************ End of Algorithm *****************************/
 
 #include <iostream>
@@ -53,12 +61,11 @@ struct Name {
     int index;
 };
 
-// prototypes:
+// Prototypes:
 bool checkEmptyFile(fstream &fin);
 void promptUserFile(string &iFileName);
-void openFileName(string &iFileName, fstream &fin);
 void readInfo(fstream &fin, Bank &BankInfo, Customer Info[]);
-void validateFileName(fstream &fin,Bank &BankInfo, Customer Info[]);
+void validateFileName(fstream &fin, Bank &BankInfo, Customer Info[], string iFileName);
 int countCustomer(Customer Info[]);
 void pauseCls();
 void addCustomer(Customer Info[], Bank &BankInfo, int counter);
@@ -76,7 +83,22 @@ void sortCustomers(Customer Info[]);
 void showMenu(char menu[]);
 void chooseMenu(Customer Info[], Bank &BankInfo);
 
-// functions
+// Functions:
+
+int main()
+{
+    string iFileName;
+    Customer Info[MAX];
+    Bank BankInfo;
+    fstream fin;
+
+    promptUserFile(iFileName);
+    validateFileName(fin, BankInfo, Info, iFileName);
+    pauseCls();
+    chooseMenu(Info, BankInfo);
+
+    return 0;
+}
 
 // check file if it is empty
 bool checkEmptyFile(fstream &fin)
@@ -93,18 +115,11 @@ void promptUserFile(string &iFileName)
     cout << endl;
 }
 
-// open the file name
-void openFileName(string &iFileName, fstream &fin)
-{
-    fin.open(iFileName.c_str());
-}
-
 void readInfo(fstream &fin, Bank &BankInfo, Customer Info[])
 {
     int counter = 0;
     while (fin.good()) {
-        if (counter == 0)
-        {
+        if (counter == 0) {
             getline(fin, BankInfo.bankName);
         }
         getline(fin, Info[counter].fullName);
@@ -117,62 +132,60 @@ void readInfo(fstream &fin, Bank &BankInfo, Customer Info[])
         BankInfo.totalAssets += Info[counter].balance;
         counter++;
     }
+    //output the total number of customer/s added to the bank from the input file
+    cout << "Successfully added " << counter << " customers to Bank: " << BankInfo.bankName;
+
+    //close the file
+    fin.close();
 }
 
 // validates the input file name
-void validateFileName(fstream &fin, Bank &BankInfo, Customer Info[])
+void validateFileName(fstream &fin, Bank &BankInfo, Customer Info[], string iFileName)
 {
-    int counter = 0;
+    // open the file name
+    fin.open(iFileName.c_str());
 
-    if (fin && !checkEmptyFile(fin))
-    {
+    if (fin && !checkEmptyFile(fin)) {
         readInfo(fin, BankInfo, Info);
-        cout << "Successfully added " << counter << " customers to Bank: " << BankInfo.bankName;
     }
-    else if (checkEmptyFile(fin))
-    {
+    else if (checkEmptyFile(fin)) {
         cout << "File is empty!";
 
         exit(0);
     }
-    else if (!fin.eof())
-    {
+    else if (!fin.eof()) {
         // If end-of-file not reached
         cout << "Error reading the file" << endl;
 
         exit(0);
     }
-    else
-    {
+    else {
         // if file couldn't open
         cout << "Couldn't read the file" << endl;
 
         exit(0);
     }
-    fin.close();
 }
 
 // counts how many customer
 int countCustomer(Customer Info[])
 {
     int count = 0;
-    for (int x = 0; x < MAX; x++)
-    {
+    for (int x = 0; x < MAX; x++) {
         if (Info[x].fullName != "")
             count++;
     }
     return count;
 }
 
-// a system pause every after a function that has been done
-void pauseCls()
-{
+// creates a system pause every after a function that has been done
+void pauseCls() {
     cout << endl << endl;
     system("pause");
     system("cls");
 }
 
-// add customer to the current file name
+// add customer to the current bank
 void addCustomer(Customer Info[], Bank &BankInfo)
 {
 
@@ -198,7 +211,7 @@ void addCustomer(Customer Info[], Bank &BankInfo)
 
 }
 
-// remove a specific customer from the file name
+// remove a specif customer from the current bank
 void removeCustomer(Customer Info[], Bank &BankInfo)
 {
     string acctName;
@@ -211,17 +224,14 @@ void removeCustomer(Customer Info[], Bank &BankInfo)
     int acctId = checkCustomerName(acctName, Info).index;
     int i = countCustomer(Info);
 
-    if (checkCustomerName(acctName, Info).existingName)
-    {
+    if (checkCustomerName(acctName, Info).existingName) {
         BankInfo.totalAssets -= Info[acctId].balance;
-        for (int x = acctId; x < i; ++x)
-        {
+        for (int x = acctId; x < i; ++x) {
             Info[x] = Info[x + 1];
         }
         cout << "Successfully removed customer from the current Bank" << endl;
     }
-    else
-    {
+    else {
         cout << "Account name doesn't exist!";
     }
     pauseCls();
@@ -235,8 +245,7 @@ void displayTableFormat(Customer Info[], Bank &BankInfo)
     cout << BankInfo.bankName << " Report" << endl;
     cout << "Name \t\tBalance" << endl;
     cout << "--------------------------------" << endl;
-    for (int x = 0; x < i; x++)
-    {
+    for (int x = 0; x < i; x++) {
         cout << setprecision(2) << fixed;
         cout << Info[x].fullName << "\t" << "$" << Info[x].balance << endl;
     }
@@ -246,16 +255,14 @@ void displayTableFormat(Customer Info[], Bank &BankInfo)
     pauseCls();
 }
 
-// check if it is an existing customer in the file name
+// check if it is an existing customer in the Bank
 Name checkCustomerName(string acctName, Customer Info[])
 {
     Name cName;
     int i = countCustomer(Info);
 
-    for (int x = 0; x < i; x++)
-    {
-        if (acctName == Info[x].fullName)
-        {
+    for (int x = 0; x < i; x++) {
+        if (acctName == Info[x].fullName) {
             cName.existingName = true;
             cName.index = x;
         }
@@ -275,24 +282,20 @@ void withdraw(Customer Info[], Bank &BankInfo)
 
     int acctId = checkCustomerName(acctName, Info).index;
 
-    if (!checkCustomerName(acctName, Info).existingName)
-    {
+    if (!checkCustomerName(acctName, Info).existingName) {
         cout << "Account name doesn't exist!" << endl;
     }
-    else
-    {
+    else {
         double withdrawAmount = 0;
         cout << "Account Name: \t" << Info[acctId].fullName << endl;
         cout << setprecision(2) << fixed;
         cout << "Available Balance: \t" << "$" << Info[acctId].balance << endl;
         cout << "Withdraw Amount: ";
         cin >> withdrawAmount;
-        if (withdrawAmount > Info[acctId].balance)
-        {
+        if (withdrawAmount > Info[acctId].balance) {
             cout << "Insufficient funds.";
         }
-        else
-        {
+        else {
             Info[acctId].balance = Info[acctId].balance - withdrawAmount;
             BankInfo.totalAssets -= withdrawAmount;
             cout << endl;
@@ -316,24 +319,20 @@ void deposit(Customer Info[], Bank &BankInfo)
 
     int acctId = checkCustomerName(acctName, Info).index;
 
-    if (!checkCustomerName(acctName, Info).existingName)
-    {
+    if (!checkCustomerName(acctName, Info).existingName) {
         cout << "Account name doesn't exist!" << endl;
     }
-    else
-    {
+    else {
         double depositAmount = 0;
         cout << "Account Name: \t" << Info[acctId].fullName << endl;
         cout << setprecision(2) << fixed;
         cout << "Available Balance: \t" << "$" << Info[acctId].balance << endl;
         cout << "Deposit Amount: ";
         cin >> depositAmount;
-        if (depositAmount < 0)
-        {
+        if (depositAmount < 0) {
             cout << "Invalid amount.";
         }
-        else
-        {
+        else {
             Info[acctId].balance = Info[acctId].balance + depositAmount;
             BankInfo.totalAssets += depositAmount;
             cout << endl;
@@ -356,8 +355,7 @@ void viewCustomerDetails(Customer Info[])
 
     int acctId = checkCustomerName(acctName, Info).index;
 
-    if (checkCustomerName(acctName, Info).existingName)
-    {
+    if (checkCustomerName(acctName, Info).existingName) {
         cout << "Name\t: " << Info[acctId].fullName << endl;
         cout << "Address\t: " << Info[acctId].streetAddress << endl;
         cout << "City\t: " << Info[acctId].city << endl;
@@ -365,8 +363,7 @@ void viewCustomerDetails(Customer Info[])
         cout << setprecision(2) << fixed;
         cout << "Balance\t: " << "$" << Info[acctId].balance;
     }
-    else
-    {
+    else {
         cout << "Account name doesn't exist!";
     }
     pauseCls();
@@ -376,15 +373,14 @@ void viewCustomerDetails(Customer Info[])
 void saveBankInfoToFile(Customer Info[], Bank &BankInfo)
 {
     ofstream fout;
-    string ifileName;
+    string oFileName;
 
     cout << "Please enter output file name: ";
-    cin >> ifileName;
-    fout.open(ifileName.c_str());
+    cin >> oFileName;
+    fout.open(oFileName.c_str());
 
     //ADD: Check if file name is valid; FIX: Alignment
-    if (fout)
-    {
+    if (fout) {
         int i = countCustomer(Info);
         fout << BankInfo.bankName << " Report" << endl;
         fout << "Name\t Address\t City\t State\t Balance" << endl;
@@ -411,12 +407,10 @@ void saveUpdateInputFile(Customer Info[], Bank &BankInfo)
     cin >> fileName;
     fout.open(fileName.c_str());
 
-    if (fout)
-    {
+    if (fout) {
         int i = countCustomer(Info);
         fout << BankInfo.bankName;
-        for (int x = 0; x < i; x++)
-        {
+        for (int x = 0; x < i; x++) {
             fout << endl;
             fout << Info[x].fullName << endl;
             fout << Info[x].streetAddress << endl;
@@ -425,7 +419,7 @@ void saveUpdateInputFile(Customer Info[], Bank &BankInfo)
             fout << Info[x].balance;
         }
         cout << "Successfully saved Bank Info into file." << endl;
-        cout << "Goodbye...";
+        cout << "Goodbye World...";
     }
 }
 
@@ -495,29 +489,21 @@ void sortCustomers(Customer Info[])
     cout << "Choose a letter from the menu: " << flush;
     cin >> option;
 
-    if (option[1] != NULL)
-    {
+    if (option[1] != NULL) {
         cout << "Please try again." << endl;
     }
-    else
-    {
+    else {
         char o = toupper(option[0]);
-        switch (o) {
-            case 'N':
-                bubbleSort(Info, o);
-                break;
-            case 'C':
-                bubbleSort(Info, o);
-                break;
-            case 'B':
-                bubbleSort(Info, o);
-                break;
-            default:
-                cout << "Please choose a letter from the menu." << endl;
+        if (o == 'N' || o == 'C' || o == 'B') {
+            bubbleSort(Info, o);
+        }
+        else {
+            cout << "Please choose a letter from the menu." << endl;
         }
     }
     pauseCls();
 }
+
 
 void showMenu(char menu[])
 {
@@ -545,12 +531,10 @@ void chooseMenu(Customer Info[], Bank &BankInfo)
         showMenu(menu);
         system("cls");
         // if second index is not empty outputs an error message
-        if (menu[1] != NULL)
-        {
+        if (menu[1] != NULL) {
             cout << "Please try again." << endl;
         }
-        else
-        {
+        else {
             switch (toupper(menu[0])) {
                 case 'A':
                     addCustomer(Info, BankInfo);
@@ -584,20 +568,4 @@ void chooseMenu(Customer Info[], Bank &BankInfo)
             }
         }
     } while (menu[0] != 'X');
-}
-
-int main()
-{
-    string iFileName, oFileName;
-    Customer Info[MAX];
-    Bank BankInfo;
-    fstream fin;
-
-    promptUserFile(iFileName);
-    openFileName(iFileName, fin);
-    validateFileName(fin, BankInfo, Info);
-    pauseCls();
-    chooseMenu(Info, BankInfo);
-
-    return 0;
 }
